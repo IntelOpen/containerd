4f124bddd22f6c477d41b42e2bc2c5d34763d8f3
Zhang Kang <kang.zhang@intel.com>
2022-07-15 22:38:25 +0800

    add tc

--- pkg/cri/server/container_create_linux.go
+++ pkg/cri/server/container_create_linux.go
@@ -20,6 +20,8 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 	"io"
 	"os"
 	"strconv"
@@ -55,6 +57,12 @@ const (
 	// seccompDefaultProfile is the default seccomp profile.
 	seccompDefaultProfile = dockerDefault
 )
+
+type info struct {
+	iface string
+	bw    uint64
+	sec   int64
+}
 
 // containerMounts sets up necessary container system file mounts
 // including /dev/shm, /etc/hosts and /etc/resolv.conf.
@@ -300,6 +308,104 @@ func (c *criService) containerSpec(
 		if linuxNetworkIO, err := networkIOToLinuxOci(networkIOClass); err == nil {
 			// fmt.Printf("***linuxNetworkIO: %d***\n", *linuxNetworkIO.ClassID)
 			specOpts = append(specOpts, oci.WithNetworkIO(linuxNetworkIO))
+			eth0 := getDefaultNIC()
+			i := &info{}
+			i.iface = eth0
+			i.bw = 125000
+			i.sec = 10
+			l, err := netlink.LinkByName(eth0)
+			if err != nil {
+				fmt.Println("-------------zk-----------%s\n", err)
+			}
+			qdiscs, err := safeQdiscList(l)
+			if err != nil {
+				fmt.Println("-------------zk list qdisc-----------%s\n", err)
+			}
+
+			var htb *netlink.Htb
+			var hasHtb = false
+			for _, qdisc := range qdiscs {
+				fmt.Println("qdisc is %s\n", qdisc)
+
+				h, isHTB := qdisc.(*netlink.Htb)
+				if isHTB {
+					htb = h
+					hasHtb = true
+					break
+				}
+			}
+
+			if !hasHtb {
+				// qdisc
+				// tc qdisc add dev lo root handle 1:0 htb default 1
+				attrs := netlink.QdiscAttrs{
+					LinkIndex: l.Attrs().Index,
+					Handle:    netlink.MakeHandle(1, 0),
+					Parent:    netlink.HANDLE_ROOT,
+				}
+				htb = netlink.NewHtb(attrs)
+				err = netlink.QdiscAdd(htb)
+				if err != nil {
+					fmt.Println("QdiscAdd error: %s\n", err)
+				}
+			}
+
+			// htb parent class
+			// tc class add dev lo parent 1:0 classid 1:1 htb rate 125Mbps ceil 125Mbps prio 0
+			// preconfig
+			classattrs1 := netlink.ClassAttrs{
+				LinkIndex: l.Attrs().Index,
+				Parent:    netlink.MakeHandle(1, 0),
+				Handle:    netlink.MakeHandle(1, 1),
+			}
+			htbclassattrs1 := netlink.HtbClassAttrs{
+				Rate:    10000000000,
+				Cbuffer: 0,
+			}
+			class1 := netlink.NewHtbClass(classattrs1, htbclassattrs1)
+			if err := netlink.ClassAdd(class1); err != nil {
+				fmt.Println("Class add error: ", err)
+			}
+
+			// htb child class
+			// tc class add dev lo parent 1:0 classid 1:5 htb rate 125kbps ceil 250kbps prio 0
+			classattrs2 := netlink.ClassAttrs{
+				LinkIndex: l.Attrs().Index,
+				Parent:    netlink.MakeHandle(1, 0),
+				Handle:    netlink.MakeHandle(1, 5),
+			}
+			htbclassattrs2 := netlink.HtbClassAttrs{
+				Rate:    i.bw,
+				Cbuffer: uint32(i.bw) * 2,
+			}
+			class2 := netlink.NewHtbClass(classattrs2, htbclassattrs2)
+			if err := netlink.ClassAdd(class2); err != nil {
+				fmt.Println("lass add error", err)
+			}
+
+			// filter add
+			// tc filter add dev lo parent 1:0 prio 0 protocol all handle 5 fw flowid 1:5
+			filterattrs := netlink.FilterAttrs{
+				LinkIndex: l.Attrs().Index,
+				Parent:    netlink.MakeHandle(1, 0),
+				Handle:    netlink.MakeHandle(0, 1024),
+				Priority:  49152,
+				Protocol:  unix.ETH_P_ALL,
+			}
+
+			fwattrs := netlink.FilterFwAttrs{
+				ClassId: netlink.MakeHandle(1, 5),
+			}
+
+			filter, err := netlink.NewFw(filterattrs, fwattrs)
+			if err != nil {
+				fmt.Println("failed to create NewFw(). Reason:%s", err)
+			}
+
+			if err := netlink.FilterAdd(filter); err != nil {
+				fmt.Println("failed to add filter. Reason:%s", err)
+			}
+
 		} else {
 			return nil, err
 		}
--- pkg/cri/server/networkio_linux.go
+++ pkg/cri/server/networkio_linux.go
@@ -20,12 +20,24 @@
 package server
 
 import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
 	"github.com/intel/goresctrl/pkg/blockio"
 	runtimespec "github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/vishvananda/netlink"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const (
 	ContainerNetworkIOLimit = "intel.com/networkio.limits"
+	file                    = "/proc/net/route"
+	line                    = 1    // line containing the gateway addr. (first line: 0)
+	sep                     = "\t" // field separator
+	field                   = 2    // field containing hex gateway address (first field: 0)
 )
 
 // blockIOClassFromAnnotations examines container and pod annotations of a
@@ -39,4 +51,58 @@ func (c *criService) networkIOClassFromAnnotations(containerName string, contain
 // structure in the OCI runtime spec.
 func networkIOToLinuxOci(className string) (*runtimespec.LinuxNetwork, error) {
 	return blockio.OciLinuxNetworkIO(className)
+}
+
+func getDefaultNIC() string {
+	file, err := os.Open(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var eth0 string
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	// jump to line containing the gateway address
+	for i := 0; i < line; i++ {
+		scanner.Scan()
+	}
+
+	// get field containing gateway address
+	tokens := strings.Split(scanner.Text(), sep)
+	eth0 = tokens[0]
+	fmt.Println(tokens[0])
+	gatewayHex := "0x" + tokens[field]
+
+	// cast hex address to uint32
+	d, _ := strconv.ParseInt(gatewayHex, 0, 64)
+	d32 := uint32(d)
+
+	// make net.IP address from uint32
+	ipd32 := make(net.IP, 4)
+	binary.LittleEndian.PutUint32(ipd32, d32)
+	fmt.Printf("%T --> %[1]v\n", ipd32)
+
+	// format net.IP to dotted ipV4 string
+	ip := net.IP(ipd32).String()
+	fmt.Printf("%T --> %[1]v\n", ip)
+	return eth0
+}
+
+func safeQdiscList(link netlink.Link) ([]netlink.Qdisc, error) {
+	qdiscs, err := netlink.QdiscList(link)
+	if err != nil {
+		return nil, err
+	}
+	result := []netlink.Qdisc{}
+	for _, qdisc := range qdiscs {
+		// filter out pfifo_fast qdiscs because
+		// older kernels don't return them
+		_, pfifo := qdisc.(*netlink.PfifoFast)
+		if !pfifo {
+			result = append(result, qdisc)
+		}
+	}
+	return result, nil
 }
